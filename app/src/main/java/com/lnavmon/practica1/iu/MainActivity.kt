285f6db636191699ec4cf600a5c3c0fd76f22f2d
LuisNavarroMontes <luis.navarro.montes@gmail.com>
2023-02-20 19:28:31 +0100

    Paso 4

--- app/src/main/java/com/lnavmon/practica1/iu/MainActivity.kt
+++ app/src/main/java/com/lnavmon/practica1/iu/MainActivity.kt
@@ -2,14 +2,23 @@ package com.lnavmon.practica1.iu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.lnavmon.practica1.R
+
 import com.lnavmon.practica1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+        setSupportActionBar(binding.materialToolbar)
+        setupActionBarWithNavController(navController, AppBarConfiguration(setOf(R.id.newQuotationFragment, R.id.aboutDialogFragment,R.id.favouritesFragment,R.id.settingsFragment)))
+
     }
 }
