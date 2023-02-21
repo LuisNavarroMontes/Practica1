ab82a09e1673a9f298c79896216ee90dcc0ad045
LuisNavarroMontes <luis.navarro.montes@gmail.com>
2023-02-21 14:03:10 +0100

    Paso 6 casi acabado

--- app/src/main/java/com/lnavmon/practica1/iu/MainActivity.kt
+++ app/src/main/java/com/lnavmon/practica1/iu/MainActivity.kt
@@ -2,23 +2,45 @@ package com.lnavmon.practica1.iu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.core.view.MenuProvider
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationBarView
 import com.lnavmon.practica1.R
 
 import com.lnavmon.practica1.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MenuProvider {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.bottomNavigationView as NavigationBarView
         val navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
         binding.bottomNavigationView.setupWithNavController(navController)
         setSupportActionBar(binding.materialToolbar)
         setupActionBarWithNavController(navController, AppBarConfiguration(setOf(R.id.newQuotationFragment, R.id.aboutDialogFragment,R.id.favouritesFragment,R.id.settingsFragment)))
 
     }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_about, menu)
+
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        addMenuProvider(this)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigationView as NavigationBarView
+        val navController = binding.fragmentContainerView.getFragment<NavHostFragment>().navController
+        navController.navigate(R.id.aboutDialogFragment)
+        return true
+    }
+
 }
--- app/src/main/java/com/lnavmon/practica1/iu/abort/AboutDialogFragment.kt
+++ app/src/main/java/com/lnavmon/practica1/iu/abort/AboutDialogFragment.kt
@@ -3,5 +3,5 @@ package com.lnavmon.practica1.iu.abort
 import androidx.fragment.app.DialogFragment
 import com.lnavmon.practica1.R
 
-class AboutDialogFragment:DialogFragment(R.layout.fragment_abort) {
+class AboutDialogFragment:DialogFragment(R.layout.fragment_about) {
 }
